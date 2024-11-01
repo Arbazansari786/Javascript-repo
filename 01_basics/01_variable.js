f69e1b32305a3b30663e1c859781cd0d7d33e103
Arbazansari786 <64585022+Arbazansari786@users.noreply.github.com>
2024-11-01 12:29:39 +0000

    variables_basics

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId=1276
+let accountEmail="ansariarbaz@gmail.com"
+var accountPassword="12345"
+accountCity="Mumbai"
+let accountState=""
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+    1.const is used for declaring constants
+    2.let is used to declare variable which are block scope
+    3.var is used to declare variables which are non block scope
+    
+    Note1 : do not use var since it's give problem in block scope & functional scope
+    Note2 : if we have not initalized the variables in JS the default value is undefined
+
+*/ 
