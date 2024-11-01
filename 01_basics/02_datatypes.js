6c7d0a03cb0b2783626dd60daf4c3bd08dd64f14
Arbazansari786 <64585022+Arbazansari786@users.noreply.github.com>
2024-11-01 12:47:59 +0000

    datatypes_basic

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3);  we are using nodejs, not browser
+
+let age=23
+let name ="Arbaz"
+
+console.log(typeof null);   //object
+
+console.log(typeof undefined);  // undefined
+
+
+
+
+
+
+/*
+    1. Primitive Types
+        Number -> 2 to power 53
+        bigInt
+        string -> can be deined in single quotes or double quotes
+        boolean -> true/false
+        null -> standalone value empty data 
+        undefined -> not initialized
+        symbol -> used for uniqueness
+
+    2. Object Type 
+*/
